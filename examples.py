396b33cff8bc3c8611c7bab04deeac58f9b1eaa6
ferandosanchez1128 <fernandosanchez1128@gmail.com>
2016-10-05 21:32:49 -0500

    clustering integrado con elastic e implementacion de lda clustering extraccion de palabras claves

--- examples.py
+++ examples.py
@@ -20,11 +20,14 @@ from pyspark.ml.feature import DCT
 from pyspark.ml.linalg import Vectors
 from pyspark.ml.feature import DCT
 from pyspark.sql.types import ArrayType, StringType
+from collections import OrderedDict
+from pyspark import SparkContext
 
 import Stemmer
 # -*- coding: utf-8 -*-
 import codecs;
-
+sc = SparkContext(appName="KMeans")	
+#~ 
 spark = SparkSession\
     .builder\
     .appName("PythonSQL")\
@@ -45,7 +48,12 @@ from pyspark.sql.functions import udf
 def stemming(palabras):
 	stemmer = Stemmer.Stemmer('english')
 	return stemmer.stemWords(palabras)
-    
+
+def words (indices):
+	palabras = []
+	for index in indices:
+		palabras.append (vocabulario[index])
+	return palabras
  	
 def search2():
     query = es.search(index="prueba",scroll='2m',size =2000,
@@ -71,64 +79,91 @@ def search2():
     print "fin"
     return response
 
+def convert_to_row(d):
+    text =  d[1]['content']
+    return Row(label = '2', sentence = text)
+
 #~ 
- 
-sentenceData = spark.createDataFrame([
-    (1, "I wish Java could use case classes 0"),
-    (2, "I wish Java could use case classes"),
-    (3, "I wish Java could use case classes 2"),
-    (4, "Esto es un texto de prueba"),
-    (5, "Esto es un texto de prueba 2"),
-    (6, "I wish Java could use case classes 3"),
-    (7, "I wish Java could use case classes 4"),
-    (8, "Esto es un texto de prueba 3"),
-    (9, "Esto es un texto de prueba 4"),
-    (10, "I wish Java could use case classes 5"),
-    (11, "I wish Java could use case classes fndjf djcdkanjan djfnadkf djfakf akf"),
-    (12, "hablando en espaniol aunque hay diversas formas del espaniol solo digo eso esto es una prueba"),
-    (13, "I only wish than this test be ok on all")
-], ["label", "sentence"])
+ #~ 
+#~ sentenceData = spark.createDataFrame([
+    #~ (1, "I wish Java could use case classes 0"),
+    #~ (2, "I wish Java could use case classes"),
+    #~ (3, "I wish Java could use case classes 2"),
+    #~ (4, "Esto es un texto de prueba"),
+    #~ (5, "Esto es un texto de prueba 2"),
+    #~ (6, "I wish Java could use case classes 3"),
+    #~ (7, "I wish Java could use case classes 4"),
+    #~ (8, "Esto es un texto de prueba 3"),
+    #~ (9, "Esto es un texto de prueba 4"),
+    #~ (10, "I wish Java could use case classes 5"),
+    #~ (11, "I wish Java could use case classes fndjf djcdkanjan djfnadkf djfakf akf"),
+    #~ (12, "hablando en espaniol aunque hay diversas formas del espaniol solo digo eso esto es una prueba"),
+    #~ (13, "I only wish than this test be ok on all")
+#~ ], ["label", "sentence"])
 
 #~ data = search2()
 #~ print len(data)
 #~ sentenceData = spark.createDataFrame(data, ["label", "sentence"])
 
-tokenizer = Tokenizer(inputCol="sentence", outputCol="words_complete")
+conf = {"es.resource" : "prueba", "es.nodes" : "127.0.0.1"}
+
+
+rdd = sc.newAPIHadoopRDD("org.elasticsearch.hadoop.mr.EsInputFormat",
+"org.apache.hadoop.io.NullWritable", 
+"org.elasticsearch.hadoop.mr.LinkedMapWritable", conf=conf)
+
+#sentenceData = sc.parallelize([{"arg1": "", "arg2": ""},{"arg1": "", "arg2": ""},{"arg1": "", "arg2": ""}]).toDF()
+rowData = rdd.map(convert_to_row)
+sentenceData = spark.createDataFrame(rowData)
+sentenceData.show()
+
+
+tokenizer = RegexTokenizer(inputCol="sentence", outputCol="words_complete", pattern="\\W")
 wordsData = tokenizer.transform(sentenceData)
 remover = StopWordsRemover(inputCol="words_complete", outputCol="words")
 dataCleaned = remover.transform(wordsData)
 udfStemming=udf(stemming, ArrayType( StringType() ))
 dataCleaned = dataCleaned.withColumn("stemm", udfStemming("words"))
-#dataCleaned.show ()
 
 
-#~ 
-word2Vec = Word2Vec(inputCol="stemm", outputCol="features",vectorSize=15)#vectorSize=100
+
+#---------------------------------------------------------------------------------------
+#usado para el clustering con kmeans
+word2Vec = Word2Vec(inputCol="stemm", outputCol="features")#vectorSize=100
 model = word2Vec.fit(dataCleaned)
 rescaledData = model.transform(dataCleaned)
-#model.getVectors().show()
+rescaledData.show()
+#---------------------------------------------------------------------------------------
+
 
 #rescaledData.select('stemm','features').show(truncate=False)
 
 #~ 
 
-#~ hashingTF = HashingTF(inputCol="stemm", outputCol="features2", numFeatures=2) #numero de palabras
+#~ hashingTF = HashingTF(inputCol="stemm", outputCol="features2", numFeatures=100) #numero de palabras
 #~ featurizedData = hashingTF.transform(dataCleaned)
-#~ # alternatively, CountVectorizer can also be used to get term frequency vectors
-#~ 
-#~ 
-#~ idf = IDF(inputCol="features2", outputCol="features")	
-#~ idfModel = idf.fit(featurizedData)
-#~ rescaledData = idfModel.transform(featurizedData)
-#~ rescaledData.select('words','stemm','features').show(truncate=False)
+#~ hashingTF.vocabulary
+# alternatively, CountVectorizer can also be used to get term frequency vectors
 
-#~ normalizer = Normalizer(inputCol="featuresL", outputCol="features", p=100.0)
-#~ rescaledData = normalizer.transform(rescaledData2)
+#---------------------------------------------------------------------------------------
+#usado para el clustering de topics
+cv = CountVectorizer(inputCol="stemm", outputCol="features2")
+model = cv.fit(dataCleaned)
+featurizedData = model.transform(dataCleaned)
+vocabulario =  model.vocabulary
+
+idf = IDF(inputCol="features2", outputCol="features")	
+idfModel = idf.fit(featurizedData)
+rescaledData = idfModel.transform(featurizedData)
+#---------------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------------
 #inicio de clustering usando kmeans
-r = rescaledData.cache()
-kmeans = KMeans(k=2, seed=10)
-model = kmeans.fit(r)
-wssse = model.computeCost(r)
+#~ r = rescaledData.cache()
+#~ kmeans = KMeans(k=2, seed=10,initMode = "k-means||")
+#~ model = kmeans.fit(r)
+#~ wssse = model.computeCost(r)
 
 #~ centers = model.clusterCenters()
 #~ transformed = model.transform(rescaledData) 
@@ -140,7 +175,7 @@ wssse = model.computeCost(r)
 	#~ 
 #~ instancias = transformed.groupBy("prediction","label").count().orderBy("count",ascending=False)
 #~ instancias.show()
-#~ 
+
 #~ 
 #~ print("Within Set Sum of Squared Errors = " + str(wssse))
 #a = instancias.withColumn('algo', lit (model.computeCost(transformed.filter(transformed.prediction == instancias['prediction']))))
@@ -169,33 +204,35 @@ wssse = model.computeCost(r)
     
 
 #----------------------------------------------------------------------
-#inicio de clustering usando latent
-#~ lda = LDA(k=2)
-#~ model = lda.fit(rescaledData)
-#~ 
-#~ ll = model.logLikelihood(rescaledData)
-#~ lp = model.logPerplexity(rescaledData)
-#~ 
-#~ print("The lower bound on the log likelihood of the entire corpus: " + str(ll))
-#~ print("The upper bound bound on perplexity: " + str(lp))
-#~ 
-#~ 
-#~ 
-#~ # Describe topics.
-#~ topics = model.describeTopics(2)
-#~ print("The topics described by their top-weighted terms:")
-#~ topics.show(truncate=False)
-#~ index = topics.select('termIndices').collect()
+#~ #inicio de clustering usando latent
+lda = LDA(k=2)
+model = lda.fit(rescaledData)
+
+ll = model.logLikelihood(rescaledData)
+lp = model.logPerplexity(rescaledData)
+
+print("The lower bound on the log likelihood of the entire corpus: " + str(ll))
+print("The upper bound bound on perplexity: " + str(lp))
+
+
+
+# Describe topics.
+topics = model.describeTopics(10)
+print("The topics described by their top-weighted terms:")
+
+udfWords=udf(words, ArrayType( StringType() ))
+topics_words = topics.withColumn("words", udfWords("termIndices"))
+topics_words.show(truncate=False)
 
 
 # Shows the result
-#transformed = model.transform(rescaledData)
-#transformed.select("features").show(truncate=False)
+transformed = model.transform(rescaledData)
+transformed	.show()
 
 #----------------------------------------------------------------------	
 
-#gaussian clustering
-#~ gmm = GaussianMixture().setK(10)
+#~ #gaussian clustering
+#~ gmm = GaussianMixture().setK(2)
 #~ model = gmm.fit(rescaledData)
 #~ 
 #~ print("Gaussians: ")
