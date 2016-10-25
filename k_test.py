83e645035a06ead1210d294fd6f0c7bcbf51500e
ferandosanchez1128 <fernandosanchez1128@gmail.com>
2016-10-25 00:52:27 -0500

    algoritmos de clustering integrados e implementacion del logging

---
+++ k_test.py
@@ -0,0 +1,203 @@
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer, RegexTokenizer,StopWordsRemover,CountVectorizer
+from pyspark.ml.clustering import BisectingKMeans
+from pyspark.ml.feature import Word2Vec	
+from pyspark.ml.clustering import KMeans
+from pyspark.sql import SparkSession, Row	
+#from texto import texto
+from pyspark.ml.clustering import LDA
+from pandas.tools.plotting import parallel_coordinates
+from pyspark.sql.functions import lit
+from pyspark.ml.feature import Normalizer
+from pyspark.sql.functions import exp
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas.tools.plotting import scatter_matrix
+import random 
+import numpy 
+from elasticsearch import Elasticsearch
+from pyspark.ml.clustering import GaussianMixture
+from pyspark.ml.feature import DCT
+from pyspark.ml.linalg import Vectors
+from pyspark.ml.feature import DCT
+from pyspark.sql.types import ArrayType, StringType
+from collections import OrderedDict
+from pyspark import SparkContext
+from pyspark.storagelevel import StorageLevel
+from pyspark.ml.feature import MaxAbsScaler
+from pyspark.ml.feature import MinMaxScaler
+import Stemmer
+# -*- coding: utf-8 -*-
+import codecs;
+sc = SparkContext(appName="KMeans")	
+#~ 
+spark = SparkSession\
+    .builder\
+    .appName("PythonSQL")\
+    .config("spark.sql.inMemoryColumnarStorage.compressed", "true")\
+    .getOrCreate()
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer
+es = Elasticsearch("127.0.0.1")
+#
+
+
+
+
+from pyspark.sql.functions import udf
+def stemming(palabras):
+	stemmer = Stemmer.Stemmer('english')
+	return stemmer.stemWords(palabras)
+
+def words (indices):
+	palabras = []
+	for index in indices:
+		palabras.append (vocabulario[index])
+	return palabras
+ 	
+def convert_to_row(d):
+    text =  d[1]['content'] 
+    nombre = d[1] ['name']	
+    return Row(label = nombre, sentence = text)
+
+#~ 
+ #~ 
+#~ sentenceData = spark.createDataFrame([
+    #~ (1, "I wish Java could use case classes 0"),
+    #~ (2, "I wish Java could use case classes"),
+    #~ (3, "I wish Java could use case classes 2"),
+    #~ (4, "Esto es un texto de prueba"),
+    #~ (5, "Esto es un texto de prueba 2"),
+    #~ (6, "I wish Java could use case classes 3"),
+    #~ (7, "I wish Java could use case classes 4"),
+    #~ (8, "Esto es un texto de prueba 3"),
+    #~ (9, "Esto es un texto de prueba 4"),
+    #~ (10, "I wish Java could use case classes 5"),
+    #~ (11, "I wish Java could use case classes fndjf djcdkanjan djfnadkf djfakf akf"),
+    #~ (12, "hablando en espaniol aunque hay diversas formas del espaniol solo digo eso esto es una prueba"),
+    #~ (13, "I only wish than this test be ok on all"),
+    #~ (14, "Spark es un framework de analisis distribuido en memoria, el cual fue desarrolado en la universidad de California < > . ' '.")
+#~ ], ["label", "sentence"])
+
+
+#sentenceData = spark.createDataFrame(data, ["label", "sentence"])
+conf = {"es.resource" : "prueba", "es.nodes" : "127.0.0.1", "es.query" : "?q=name:alt.atheism name:misc.forsale" }
+
+
+rdd = sc.newAPIHadoopRDD("org.elasticsearch.hadoop.mr.EsInputFormat",
+"org.apache.hadoop.io.NullWritable", 
+"org.elasticsearch.hadoop.mr.LinkedMapWritable", conf=conf).persist(StorageLevel.DISK_ONLY)
+
+
+#print rdd.getStorageLevel()
+rowData = rdd.map(convert_to_row)
+sentenceData = spark.createDataFrame(rowData).persist(StorageLevel.DISK_ONLY)
+print sentenceData.count()
+
+print "tokenizer"
+tokenizer = RegexTokenizer(inputCol="sentence", outputCol="words_complete", pattern="\\W")
+wordsData = tokenizer.transform(sentenceData).persist(StorageLevel.DISK_ONLY)
+sentenceData.unpersist()
+
+print "stemming"
+udfStemming=udf(stemming, ArrayType( StringType() ))
+dataStemm = wordsData.withColumn("stemm", udfStemming("words_complete")).persist(StorageLevel.DISK_ONLY)
+wordsData.unpersist()
+
+print "words remover"
+remover = StopWordsRemover(inputCol="stemm", outputCol="words",stopWords =  StopWordsRemover.loadDefaultStopWords('english'))
+dataCleaned = remover.transform(dataStemm).persist(StorageLevel.DISK_ONLY)
+dataCleaned.select ("words").show()
+dataStemm.unpersist()
+
+#---------------------------------------------------------------------------------------
+#usado para el clustering con kmeans
+#~ 
+#~ word2Vec = Word2Vec(inputCol="words", outputCol="features")
+#~ print "vectorizer1"
+#~ model = word2Vec.fit(dataCleaned)
+#~ print "vectorizer2"
+#~ rescaledData = model.transform(dataCleaned).persist(StorageLevel.DISK_ONLY)
+#~ dataCleaned.unpersist()
+#~ rescaledData.show()
+#print rescaledData.getStorageLevel()
+#---------------------------------------------------------------------------------------
+
+#~ 
+hashingTF = HashingTF(inputCol="words", outputCol="featuresCount", numFeatures=1000)#numero de palabras
+featurizedData = hashingTF.transform(dataCleaned)
+
+#---------------------------------------------------------------------------------------
+
+#usado para el clustering de topics
+#~ cv = CountVectorizer(inputCol="words", outputCol="featuresCount")#, minTF=4.0, minDF=20)
+#~ model = cv.fit(dataCleaned)
+#~ featurizedData = model.transform(dataCleaned)
+#~ vocabulario =  model.vocabulary
+
+
+
+idf = IDF(inputCol="featuresCount", outputCol="featuresInv")	
+idfModel = idf.fit(featurizedData)
+tfIdf = idfModel.transform(featurizedData)
+#~ 
+normalizer = Normalizer(inputCol="featuresInv", outputCol="features",p=2.0)
+rescaledData = normalizer.transform(tfIdf)
+
+#---------------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------------
+#inicio de clustering usando kmeans
+
+print "kmeans"
+kmeans = KMeans(k=2, seed=1,initMode="random" )
+model = kmeans.fit(rescaledData)
+wssse = model.computeCost(rescaledData)
+#~ 
+centers = model.clusterCenters()
+transformed = model.transform(rescaledData) 
+#~ #transformed.select ("features").show()   
+
+
+instancias = transformed.groupBy("prediction").count()
+instancias.show()
+#~ 
+#~ for center in centers:
+	#~ print center
+instancias = transformed.groupBy("prediction","label").count().orderBy("count",ascending=False)
+instancias.show(truncate=False)
+#~ p = instancias.collect()
+#~ print p
+    
+
+#----------------------------------------------------------------------
+#~ #inicio de clustering usando latent
+#~ lda = LDA(k=2)
+#~ model = lda.fit(rescaledData)
+#~ 
+#~ ll = model.logLikelihood(rescaledData)
+#~ lp = model.logPerplexity(rescaledData)
+#~ 
+#~ print("The lower bound on the log likelihood of the entire corpus: " + str(ll))
+#~ print("The upper bound bound on perplexity: " + str(lp))
+#~ 
+#~ # Describe topics.
+#~ topics = model.describeTopics(10)
+#~ print("The topics described by their top-weighted terms:")
+#~ 
+#~ udfWords=udf(words, ArrayType( StringType() ))
+#~ topics_words = topics.withColumn("words", udfWords("termIndices"))
+#~ topics_words.select ("words").show(truncate=False)
+
+#----------------------------------------------------------------------
+#bisecting kmeans
+#~ gmm = GaussianMixture().setK(6).setSeed(1)
+#~ model = gmm.fit(rescaledData)
+#~ transformed = model.transform(rescaledData)
+#~ transformed.show(truncate=True)
+#~ instancias = transformed.groupBy("prediction").count()	
+#~ instancias.show()
+#~ 
+#~ instancias = transformed.groupBy("prediction","label").count().orderBy("count",ascending=False)
+#~ instancias.show()
+#~ 
+
---
+++ kmeans.py
@@ -0,0 +1,234 @@
+from statsmodels.iolib.tests.test_table_econpy import row0data
+
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer, RegexTokenizer,StopWordsRemover,CountVectorizer
+from pyspark.ml.clustering import BisectingKMeans,KMeans
+from pyspark.sql import SparkSession, Row
+from pyspark.ml.feature import Word2Vec
+from pyspark.ml.clustering import LDA
+from pyspark.sql.functions import lit
+from pyspark.sql.types import ArrayType, StringType
+from collections import OrderedDict
+from pyspark import SparkContext
+from pyspark.storagelevel import StorageLevel
+
+# - *- coding: utf- 8 - *-
+import codecs;
+from pyspark import SparkContext, SparkConf
+from pyspark.ml.feature import Normalizer
+from pyspark.sql.functions import udf
+import logging
+from docs.config import *
+import Stemmer
+
+from elasticsearch import Elasticsearch
+
+class Clustering:
+    def __init__(self, algoritmo="kmeans", features ="tfidf", num_clusters=2):
+
+        logging.basicConfig(filename=LOG_FILE, level=logging.INFO,
+                            format='%(levelname)s[%(asctime)s]: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
+                            filemode='a')
+        logging.info("Archvo de log creado")
+
+        try:
+            logging.info("conectando Spark con ElasticSearch")
+            sc = SparkContext(appName="KMeans")
+            self.k = num_clusters
+            self.algoritmo = algoritmo
+            self.features = features
+            spark = SparkSession \
+                .builder \
+                .appName("PythonSQL") \
+                .config("spark.sql.inMemoryColumnarStorage.compressed", "true") \
+                .getOrCreate()
+
+            conf = {"es.resource": ELASTIC_SEARCH_INDEX, "es.nodes": ELASTIC_SEARCH_HOST,
+                    "es.query": "?q=name:alt.atheism name:misc.forsale"}
+            rdd = sc.newAPIHadoopRDD("org.elasticsearch.hadoop.mr.EsInputFormat",
+                                     "org.apache.hadoop.io.NullWritable",
+                                     "org.elasticsearch.hadoop.mr.LinkedMapWritable", conf=conf).persist(
+                StorageLevel.DISK_ONLY)
+
+            #print rdd.collect()
+            rowData = rdd.map(self.convert_to_row)
+
+            self.sentenceData = spark.createDataFrame(rowData).persist(StorageLevel.DISK_ONLY)
+            print self.sentenceData.count()
+        except:
+
+            logging.warning("Ha fallado la comunicacion entre ElasticSearch y Spark")
+
+    def convert_to_row(self, d):
+        logging.info("convirtiendo a DataFrame la data de ElasticSearch")
+        text = d[1]['content']
+        nombre = d[1]['name']
+        id = d[0]
+        return Row(label=nombre, sentence=text,id=id)
+
+    def proceso_principal(self):
+        self.preprocess()
+        self.extract_features()
+        self.clustering()
+        self.actualizar_indice()
+
+    def preprocess(self):
+        logging.info("Iniciando el preproceamiento del texto")
+
+        def stemming(palabras):
+            stemmer = Stemmer.Stemmer('english')
+            return stemmer.stemWords(palabras)
+
+        def clean(palabras):
+            cleaned = []
+            for palabra in palabras:
+                if len(palabra) > 2:
+                    cleaned.append(palabra)
+            return cleaned
+
+        try:
+            logging.info("Tokenizacion del texto")
+            tokenizer = RegexTokenizer(inputCol="sentence", outputCol="words_complete", pattern="[^a-zA-Z]")
+            wordsData = tokenizer.transform(self.sentenceData).persist(StorageLevel.DISK_ONLY)
+            self.sentenceData.unpersist()
+            wordsData.select("id").show()
+            print "limpieza"
+            udfClean = udf(clean, ArrayType(StringType()))
+            dataClean = wordsData.withColumn("cleaned", udfClean("words_complete")).persist(StorageLevel.DISK_ONLY)
+            wordsData.unpersist()
+
+            logging.info("stemming de los tokens")
+            udfStemming = udf(stemming, ArrayType(StringType()))
+            dataStemm = dataClean.withColumn("stemm", udfStemming("cleaned")).persist(StorageLevel.DISK_ONLY)
+            wordsData.unpersist()
+
+            logging.info("Eliminacion de las plabras vacias")
+            remover = StopWordsRemover(inputCol="stemm", outputCol="words",
+                                       stopWords=StopWordsRemover.loadDefaultStopWords('english'))
+            self.dataCleaned = remover.transform(dataStemm).persist(StorageLevel.DISK_ONLY)
+            self.dataCleaned.select("words").show()
+            dataStemm.unpersist()
+        except :
+            logging.warning("Ha ocurrido un fallo en el preprocesamiento del texto")
+
+
+    def extract_features(self):
+        if self.features == "tfidf":
+            self.tfidf()
+        elif self.features == "vector":
+            self.vector()
+        else:
+            self.counter()
+
+    def tfidf(self):
+        try:
+            logging.info("Creando el TF-IDF")
+            hashingTF = HashingTF(inputCol="words", outputCol="featuresCount", numFeatures=1000)  # numero de palabras
+            featurizedData = hashingTF.transform(self.dataCleaned)
+            idf = IDF(inputCol="featuresCount", outputCol="featuresInv")
+            idfModel = idf.fit(featurizedData)
+            tfIdf = idfModel.transform(featurizedData)
+
+            normalizer = Normalizer(inputCol="featuresInv", outputCol="features", p=2.0)
+            self.rescaledData = normalizer.transform(tfIdf)
+        except:
+            logging.warning("Ha ocurrido un fallo creando el TF-IDF")
+
+    def vector(self):
+        try:
+            logging.info("Creando el Vector")
+            word2Vec = Word2Vec(inputCol="words", outputCol="features")
+            model = word2Vec.fit(self.dataCleaned)
+            self.rescaledData = model.transform(self.dataCleaned).persist(StorageLevel.DISK_ONLY)
+            self.dataCleaned.unpersist()
+            self.rescaledData.show()
+
+        except:
+            logging.warning("Ha ocurrido un fallo creando el vector")
+
+    def counter(self):
+        try:
+            logging.info("Creando el Vector de conteo")
+            cv = CountVectorizer(inputCol="words", outputCol="features2", minDF=4.0)
+            model = cv.fit(self.dataCleaned)
+            featurizedData = model.transform(self.dataCleaned)
+            vocabulario = model.vocabulary
+
+            idf = IDF(inputCol="features2", outputCol="featuresL", minDocFreq=10)
+            idfModel = idf.fit(featurizedData)
+            rescaledData = idfModel.transform(featurizedData)
+            normalizer = Normalizer(inputCol="featuresL", outputCol="features",p=2.0)
+            self.rescaledData = normalizer.transform(rescaledData)
+        except:
+            logging.warning("Ha ocurrido un fallo creando el vector de conteo")
+
+
+
+
+    def clustering(self):
+        if self.algoritmo == "kmeans":
+            self.kmeans()
+        elif self.algoritmo == "bisecKmeans":
+            self.bisectingKmeans()
+        else:
+            print "hola mundo"
+
+    def bisectingKmeans (self):
+        try:
+            logging.info("Inicializando el algoritmo BisectingKmeans")
+            bkm = BisectingKMeans().setK(self.k).setSeed(1)
+            model = bkm.fit(self.rescaledData)
+            self.transformed = model.transform(self.rescaledData)
+            instancias = self.transformed.groupBy("prediction", "label").count().orderBy("count", ascending=False)
+            instancias.show(truncate=False)
+            self.transformed.show()
+        except:
+            logging.warning("algoritmo BisectingKmeans ha fallado")
+
+    def kmeans(self):
+        try:
+            kmeans = KMeans(k=2, initMode="random")
+            model = kmeans.fit(self.rescaledData)
+            wssse = model.computeCost(self.rescaledData)
+            # ~
+            centers = model.clusterCenters()
+            self.transformed = model.transform(self.rescaledData)
+            # ~ #transformed.select ("features").show()
+
+
+            instancias = self.transformed.groupBy("prediction").count()
+            instancias.show()
+            # ~
+            # ~ for center in centers:
+            # ~ print center
+            instancias = self.transformed.groupBy("prediction", "label").count().orderBy("count", ascending=False)
+            instancias.show(truncate=False)
+
+        except:
+            logging.warning("algoritmo Kmeans ha fallado")
+            
+         
+    def actualizar_indice(self):
+
+        def grupo(id, grupo):
+            es = Elasticsearch("127.0.0.1")
+            query = es.get(index="prueba", id=id)
+            documento = query['_source']
+            print documento
+            documento['cluster'] = grupo
+            response = es.update(index='prueba', doc_type="pdf", id=id, body={"doc": documento})
+            es.indices.refresh(index="prueba")
+            return "hola"
+
+
+
+
+
+        udfWords = udf(grupo, StringType())
+        topics_words = self.transformed.withColumn("grupo", udfWords("id", "prediction"))
+        topics_words.show()
+
+        return "hola"
+
+
+clustering = Clustering("kmeans", "counter", 2)
+clustering.proceso_principal()
