e52f712548c52e685941607d6da996163a65b468
ferandosanchez1128 <fernandosanchez1128@gmail.com>
2016-10-25 00:53:13 -0500

    indice de los archivos a elastic

--- indexar.py
+++ indexar.py
@@ -18,52 +18,49 @@
 from __future__ import print_function
 from operator import add
 from pyspark import SparkContext
-from os import listdir 
+from os import listdir
 # $example on$
 from pyspark.mllib.feature import Word2Vec
 from elasticsearch import Elasticsearch
+
 # $example off$
 es = Elasticsearch("127.0.0.1")
 
 if __name__ == "__main__":
-	
-    
+
     sc = SparkContext(appName="Word2VecExample")  # SparkContext
-    #clases = ["alt.atheism", "comp.graphics", "comp.os.ms-windows.misc",  "comp.sys.ibm.pc.hardware", "comp.sys.mac.hardware", "comp.windows.x", "misc.forsale","rec.autos","rec.motorcycles","rec.sport.baseball", "rec.sport.hockey","sci.crypt","sci.electronics","sci.med","sci.space", "soc.religion.christian"]
-    clases = ["talk.politics.guns", "talk.politics.mideast","talk.politics.misc","talk.religion.misc"] 
-    name = "prueba" 
-    #es.indices.delete(index=name, ignore=[400, 404])
+    # clases = ["alt.atheism", "comp.graphics", "comp.os.ms-windows.misc",  "comp.sys.ibm.pc.hardware", "comp.sys.mac.hardware", "comp.windows.x", "misc.forsale","rec.autos","rec.motorcycles","rec.sport.baseball", "rec.sport.hockey","sci.crypt","sci.electronics","sci.med","sci.space", "soc.religion.christian"]
+    #clases = ["talk.politics.guns", "talk.politics.mideast", "talk.politics.misc", "talk.religion.misc"]
+    #clases = ["alt.atheism", "misc.forsale"]
+    clases = ["comp.graphics","misc.forsale","rec.motorcycles","alt.atheism",]
+    name = "prueba"
+    es.indices.delete(index=name, ignore=[400, 404])
 
     # $example on$
+    i=0
     for clase in clases:
-		directorio = "textos/" + clase +"/"
-		documentos = [doc for doc in listdir(directorio)]
-		for doc in documentos:
-			inp = sc.textFile(directorio + doc)
-			texto = ""
-			texto= inp.map(lambda line: line).reduce(lambda x,y: x+y)
-			print (clase)
-			document = {
-				'name': clase,				# nombre del archivo o documento
-				'author': "Fernando",			# author de este archivo o documento
-				'content': texto,		# el contenido de este archivo o documento
-				'owner': "propietario"  			# propietario, usuario que crea el indice
-			}
-			response = es.index(index="prueba", doc_type= "pdf", body=document)	
-	
-	
-	
+        directorio = "textos/" + clase + "/"
+        documentos = [doc for doc in listdir(directorio)]
+        for doc in documentos:
+            inp = sc.textFile(directorio + doc)
+            texto = ""
+            texto = inp.map(lambda line: line).reduce(lambda x, y: x + y)
+            print(clase)
+            i = i+1
+            document = {
+                'id': i,
+                'name': clase,  # nombre del archivo o documento
+                'author': "Fernando",  # author de este archivo o documento
+                'content': texto,  # el contenido de este archivo o documento
+                'owner': "propietario"  # propietario, usuario que crea el indice
+            }
+            response = es.index(index="prueba", doc_type="pdf", body=document)
+
     es.indices.refresh(index="prueba")
-		
-	
-	
-    
 
-    
+    # synonyms = model.findSynonyms('china', 5)
 
-    #synonyms = model.findSynonyms('china', 5)
-
-    #for word, cosine_distance in synonyms:
+    # for word, cosine_distance in synonyms:
     #    print("{}: {}".format(word, cosine_distance))
     # $example off$
 
